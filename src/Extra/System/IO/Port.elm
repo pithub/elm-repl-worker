bd23c5fbc09cb80dc547b9da5aed576caf025a00
Pit Capitain <pit@capitain.de>
2025-03-19 21:05:25 +0100

    Remove unused code before repository split

--- src/Extra/System/IO/Port.elm
+++ src/Extra/System/IO/Port.elm
@@ -1,15 +1,5 @@
 module Extra.System.IO.Port exposing
-    ( AckPort
-    , AsyncAck
-    , AsyncApi
-    , AsyncCont
-    , AsyncLens
-    , AsyncReceiver
-    , AsyncReceiverFun
-    , AsyncSend
-    , AsyncSender
-    , AsyncSenderFun
-    , Codec
+    ( Codec
     , ReceivePort
     , SafeCodec
     , SendPort
@@ -19,11 +9,9 @@ module Extra.System.IO.Port exposing
     , SyncRequest
     , SyncRequester
     , SyncRequesterFun
+    , SyncRespond
     , SyncResponder
     , SyncResponderFun
-    , SyncRespond
-    , UnsafeCodec
-    , asyncApi
     , syncApi
     )
 
@@ -45,10 +33,6 @@ type alias SafeCodec value wire =
     Codec value value wire
 
 
-type alias UnsafeCodec value wire =
-    Codec value (Maybe value) wire
-
-
 
 -- PORTS
 
@@ -59,10 +43,6 @@ type alias SendPort msg wire =
 
 type alias ReceivePort msg wire =
     (wire -> msg) -> Sub msg
-
-
-type alias AckPort msg =
-    (() -> msg) -> Sub msg
 
 
 
@@ -184,119 +164,4 @@ syncApi :
 syncApi requestCodec responseCodec =
     { requesterFun = syncRequesterFun requestCodec responseCodec
     , responderFun = syncResponderFun requestCodec responseCodec
-    }
-
-
-
--- ASYNC SENDER
-
-
-type alias AsyncCont s =
-    () -> IO.IO s ()
-
-
-type alias AsyncLens s =
-    Lens.Lens s (Maybe (AsyncCont s))
-
-
-type alias AsyncSend s valueOut =
-    valueOut -> IO.IO s ()
-
-
-asyncSend :
-    Codec valueOut valueIn wire
-    -> AsyncLens s
-    -> SendPort (IO.IO s ()) wire
-    -> AsyncSend s valueOut
-asyncSend codec lens sendPort valueOut =
-    IO.liftCont <|
-        \cont ->
-            IO.sequence
-                [ IO.putLens lens (Just cont)
-                , IO.liftCmdIO (sendPort (codec.encode valueOut))
-                ]
-
-
-type alias AsyncAck s =
-    s -> Sub (IO.IO s ())
-
-
-asyncAck :
-    AsyncLens s
-    -> AckPort (IO.IO s ())
-    -> AsyncAck s
-asyncAck lens ackPort state =
-    ackPort <|
-        \() ->
-            case lens.getter state of
-                Just cont ->
-                    IO.sequence
-                        [ IO.putLens lens Nothing
-                        , cont ()
-                        ]
-
-                Nothing ->
-                    IO.noOp
-
-
-type alias AsyncSender s valueOut =
-    { send : AsyncSend s valueOut
-    , ack : AsyncAck s
-    }
-
-
-type alias AsyncSenderFun s valueOut wire =
-    AsyncLens s
-    -> SendPort (IO.IO s ()) wire
-    -> AckPort (IO.IO s ())
-    -> AsyncSender s valueOut
-
-
-asyncSenderFun :
-    Codec valueOut valueIn wire
-    -> AsyncSenderFun s valueOut wire
-asyncSenderFun codec lens sendPort ackPort =
-    { send = asyncSend codec lens sendPort
-    , ack = asyncAck lens ackPort
-    }
-
-
-
--- ASYNC RECEIVER
-
-
-type alias AsyncReceiver s valueIn =
-    (valueIn -> IO.IO s ()) -> Sub (IO.IO s ())
-
-
-type alias AsyncReceiverFun s valueIn wire =
-    ReceivePort (IO.IO s ()) wire
-    -> AsyncReceiver s valueIn
-
-
-asyncReceiverFun :
-    Codec valueOut valueIn wire
-    -> AsyncReceiverFun s valueIn wire
-asyncReceiverFun codec receivePort callback =
-    receivePort <|
-        \wire ->
-            callback (codec.decode wire)
-
-
-
--- ASYNC API
-
-
-type alias AsyncApi s valueOut valueIn wire =
-    { senderFun : AsyncSenderFun s valueOut wire
-    , receiverFun : AsyncReceiverFun s valueIn wire
-    }
-
-
-asyncApi :
-    Codec valueOut valueIn wire
-    -> AsyncApi s valueOut valueIn wire
-asyncApi codec =
-    { senderFun = asyncSenderFun codec
-    , receiverFun = asyncReceiverFun codec
     }
